2ffb66d32cff9bed7fdf23ae215009cd618ee4bb
Alain Lescouflair <adlescouflair@gmail.com>
2023-04-07 15:07:14 -0400

    fix mobile design

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu() {
+  
+    var nav = document.getElementById("nav-links");
+    if (nav.style.display === "flex") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "flex";
+    }
+  }
